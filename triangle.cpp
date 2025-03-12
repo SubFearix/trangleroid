cb3ae870973bdba243acc037504a58e96a3cdeee
SubFearix <a.pechenkin3@gmail.com>
2025-03-12 19:13:30 +0700

    triangle file

---
+++ triangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+int a, b, c;
+cout << "Ведите первую сторону: ";
+cin >> a;
+cout << "Ведите вторую сторону: ";
+cin >> b;
+cout << "Ведите третью сторону: ";
+cin >> c;
+if ((a+b>c && c+b>a && a+c>b) || (a>0 && b>0 && c>0)){
+int perimeter = a+b+c;
+float halfper = perimeter / 2.0;
+float spaceGer = sqrt(halfper * (halfper - a) * (halfper - b) * (halfper - c));
+bool isIsosceles = ((a == b || a == c || b == c)?true:false);
+if (isIsosceles) {
+cout << "Равнобедренный"<<endl;
+} else {cout <<"Не равнобедренный"<<endl;}
+cout << "Площадь по формуле Герона равна: "<<  spaceGer<<endl;
+cout <<"Периметр равен: "<<perimeter <<endl;
+} else {cout << "Треугольник не существует"<<endl;}
+return 0;
+}
